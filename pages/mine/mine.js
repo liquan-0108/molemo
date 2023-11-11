5d08b99ef05a88660ae7f2354d0a43a3f51fbe1b
liquan-0108 <liquan_0108@163.com>
2023-11-12 01:16:20 +0800

    完善我的页面，添加获取微信用户信息登录逻辑

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -5,21 +5,92 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userName:'',
+    headImg: 'https://img.yzcdn.cn/vant/cat.jpeg'
+  },
+  wxLogin() {
+    // wx.login({
+    //   timeout:5000,
+    //   success: function (code) {
+    //     debugger
+    //   },
+    //   fail: function (code) {
+    //     debugger
+    //   },
+    //   complete: function (code) {
+    //     debugger
+    //   },
+    // })
+    debugger
+    const _this = this
+    wx.getUserProfile({
+      desc: '么了鱼请求获取您的信息',
+      success: function (data) {
+        debugger
+        wx.setStorageSync('userInfo',JSON.stringify(data.userInfo))
+        _this.setData({
+          userName:data.userInfo.nickName,
+          headImg:data.userInfo.avatarUrl
+        })
+      },
+      fail: function (code) {
+        debugger
+      },
+      complete: function (code) {
+        debugger
+      },
+    })
   },
 
+  getPhoneNumber(){
+    wx.login({
+      timeout:5000,
+      success: function (data) {
+        debugger
+        var infor =  wx.getPhoneNumber({
+          code:data.code
+        })
+        console.log(infor)
+      },
+      fail: function (code) {
+        debugger
+      },
+      complete: function (code) {
+        debugger
+      },
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 检查登录态是否过期。 
+    const _this = this
+    wx.checkSession({
+      success (data) {
+        //session_key 未过期，并且在本生命周期一直有效
+        const cookie  = wx.getStorageSync('userInfo')
+        if(!cookie) return
+        const userInfo = JSON.parse(cookie)
+        _this.setData({
+          userName:userInfo.nickName,
+          headImg:userInfo.avatarUrl
+        })
+      },
+      fail () {
+        debugger
 
+        // session_key 已经失效，需要重新执行登录流程
+        wx.login() //重新登录
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    debugger
   },
 
   /**
